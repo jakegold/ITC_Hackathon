e6fce8f85fd35df046ff93c7cf9e9f64712dae44
Jake Goldstein <jsg525@nyu.edu>
2017-06-13 17:58:13 +0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,23 @@
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+from os import listdir
+from os.path import isfile, join
+
+app = ClarifaiApp()
+
+def get_foods():
+	filepath = '/Users/Jake/desktop/fridge_pics'
+	files = get_all_files(filepath)
+	foodList = []
+	for pic in files:
+		newFilepath = filepath + '/' + pic
+		image = [newFilepath]
+		img = app.tag_files(image, model='food-items-v1.0')
+		foodItem = (img["outputs"][0]['data']['concepts'][0]['name'])
+		foodList.append(foodItem)
+	return foodList
+
+def get_all_files(filepath):
+	all_files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
+	return(all_files)
