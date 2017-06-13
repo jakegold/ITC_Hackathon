11b6649a85e971dfad7d405da2d6475283c7efe3
Jake Goldstein <jsg525@nyu.edu>
2017-06-13 20:05:07 +0300

    Create database.py

--- database.py
+++ database.py
@@ -7,10 +7,13 @@ import sqlite3
 # 		strng = ", ".join(element)
 # 		c.execute("SELECT URL FROM recipes WHERE Ingredients=?",strng)
 # 		print c.fetchall()
+
 def queryfunc(lst):
 	conn = sqlite3.connect('database.db')
 	c = conn.cursor()
+	result = []
 	for elt in lst:
 		strng = "%".join(elt)
 		c.execute("SELECT URL FROM recipes WHERE Ingredients LIKE %?%",strng)
-		print c.fetchall()
+		result.append(c.fetchall())
+	return result
