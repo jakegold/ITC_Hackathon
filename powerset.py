dc5cfa3c0c9944f6b2b3b1e0ec9f27aed1a97c3f
Jake Goldstein <jsg525@nyu.edu>
2017-06-13 19:40:10 +0300

    Create powerset.py

--- powerset.py
+++ powerset.py
@@ -1,19 +1,5 @@
 import sqlite3
 
-#this function creates a powerset (a list of all possible subsets) from a list
-def powerset(lst):
-	x = len(lst)
-	output = []
-	for i in range(1 << x):
-		output.insert(0, [lst[j] for j in range(x) if (i & (1 << j))])
-	output.sort(key=len)
-	output.reverse()
-	output.pop()
-	for elt in output:
-		elt.sort()
-	return output
-
-
 #start of SQL code
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
