0498b1795fae0f01caae9fb0f0983d018b7d9e0d
Jake Goldstein <jsg525@nyu.edu>
2017-06-13 19:48:14 +0300

    Create database.py

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
 import sqlite3
 
-#start of SQL code
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
-
 def queryfunc(lst):
-	for elt in lst:
-		strng = ", ".join(elt)
+	conn = sqlite3.connect('database.db')
+	c = conn.cursor()
+	for element in lst:
+		strng = ", ".join(element)
 		c.execute("SELECT URL FROM recipes WHERE Ingredients=?",strng)
 		print c.fetchall()
