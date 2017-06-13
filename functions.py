918bcf25b2afa395a768d5bd7bc59c9326adf743
Jake Goldstein <jsg525@nyu.edu>
2017-06-13 21:01:14 +0300

    Create functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,6 @@
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
-from os import listdir
-from os.path import isfile, join
-import sqlite3
 
 app = ClarifaiApp()
 
@@ -24,27 +21,3 @@ def get_foods():
 def get_all_files(filepath):
 	all_files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
 	return all_files
-
-# Creates a powerset (a list of all possible subsets) from a list
-def power_set(elements):
-	x = len(elements)
-	result = []
-	for i in range(1 << x):
-		result.insert(0, [elements[j] for j in range(x) if (i & (1 << j))])
-	result.sort(key=len)
-	result.reverse()
-	result.pop()
-	for elt in result:
-		elt.sort()
-	return result
-
-# Returns a list of urls for the recipes
-def queryfunc(lst):
-	conn = sqlite3.connect('database.db')
-	c = conn.cursor()
-	result = []
-	for elt in lst:
-		strng = "%".join(elt)
-		c.execute("SELECT URL FROM recipes WHERE Ingredients LIKE %?%",strng)
-		result.append(c.fetchall())
-	return result
