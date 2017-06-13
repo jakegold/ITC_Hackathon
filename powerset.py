cc6ca585bd41169dea71237a9fe73f1b664e1176
roybot23 <roybot23@gmail.com>
2017-06-13 18:26:24 +0300

    Create powerset.py

--- powerset.py
+++ powerset.py
@@ -1,16 +1,11 @@
 import sqlite3
 
-# c.execute("INSERT INTO recipes (ID, Ingredients, URL) VALUES (3, \"test2\", \"test2\")")
-# conn.commit()
-
-
 def powerset(lst):
 	x = len(lst)
 	output = []
 	for i in range(1 << x):
 		output.insert(0, [lst[j] for j in range(x) if (i & (1 << j))])
 	return output
-
 
 ingredients = ['lettuce', 'tomato', 'carrot']
 
@@ -22,8 +17,6 @@ a.pop()
 
 for elt in a:
 	elt.sort()
-
-print a
 
 
 conn = sqlite3.connect('database.db')
