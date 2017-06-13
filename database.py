80db14d466c5749e9c12ba50694837352ba78a5f
roybot23 <roybot23@gmail.com>
2017-06-13 19:50:39 +0300

    Create database.py

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
 import sqlite3
 
+# def queryfunc(lst):
+# 	conn = sqlite3.connect('database.db')
+# 	c = conn.cursor()
+# 	for element in lst:
+# 		strng = ", ".join(element)
+# 		c.execute("SELECT URL FROM recipes WHERE Ingredients=?",strng)
+# 		print c.fetchall()
 def queryfunc(lst):
 	conn = sqlite3.connect('database.db')
 	c = conn.cursor()
-	for element in lst:
-		strng = ", ".join(element)
-		c.execute("SELECT URL FROM recipes WHERE Ingredients=?",strng)
+	for elt in lst:
+		strng = "%".join(elt)
+		c.execute("SELECT URL FROM recipes WHERE Ingredients LIKE %?%",strng)
 		print c.fetchall()
