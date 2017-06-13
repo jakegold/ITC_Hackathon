6c5e6a480ef58449aad2e92e180df726cd8f597b
Jake Goldstein <jsg525@nyu.edu>
2017-06-13 18:40:15 +0300

    Create functions.py

--- functions.py
+++ functions.py
@@ -6,7 +6,9 @@ from os.path import isfile, join
 
 app = ClarifaiApp()
 
+# Returns a set of all the ingredients in the fridge
 def get_foods():
+# 	NOT SURE WHAT THE DATA PATH IS!!
 	filepath = '/fridge_pics'
 	files = get_all_files(filepath)
 	foodList = []
@@ -18,6 +20,15 @@ def get_foods():
 		foodList.append(foodItem)
 	return foodList
 
+# Returns all of the files from a datapath
 def get_all_files(filepath):
 	all_files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
 	return all_files
+
+#Creates a powerset (a list of all possible subsets) from a list
+def power_set(elements):
+	x = len(elements)
+	result = []
+	for i in range(1 << x):
+		result.insert(0, [lst[j] for j in range(x) if (i & (1 << j))])
+	return result
