0b0225ae2d640de359ae8bcd38b8b6c3cc995cbf
Jake Goldstein <jsg525@nyu.edu>
2017-06-14 15:15:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from functions import *
 
 def run():
 	# foodList = get_foods()
-	foodList = ['chicken', 'cucumber', 'onion', 'pumpkin', 'tomato']
+	foodList = ['pineapple', 'cucumber', 'tomato']
 	url = browser(foodList)
 	urls = get_urls(url)
+	email = 'Hello! Here are some options for dinner tonight:' + '\n' + '\n'
 	for url in urls:
-		print(url)
-		print(get_ingreds(url))
+		email += extract_html_title(url) + '\n' + url + '\n'
+		ingred_list = get_ingreds(url)
+		shopping_list = what_2_buy(foodList, ingred_list)
+		email += ', \n'.join(shopping_list)
+		email += '\n \n'
+	email += 'Enjoy your dinner! \n Love, \n Your Smart Fridge'
+	send_email('roybot23@gmail.com', email)
+
 
 run()
