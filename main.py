8c2a69a342ad2bd42c9d24ebd13a7fb12ef62cfc
Jake Goldstein <jsg525@nyu.edu>
2017-06-14 13:58:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from functions import *
 
 def run():
-	foodList = get_foods()
+	# foodList = get_foods()
+	foodList = ['chicken', 'cucumber', 'onion', 'pumpkin', 'tomato']
 	url = browser(foodList)
-	print(get_urls(url))
+	urls = get_urls(url)
+	for url in urls:
+		print(url)
+		print(get_ingreds(url))
 
 run()
